f3defd9fa3e89c52b709d69423c917daad4d269e
Paul Gossman <me@paulgossman.com>
2021-02-26 10:59:10 -0500

    Register ChoreAssignment model with admin interface

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Chore, ChoreInstance
+from .models import Chore, ChoreAssignment, ChoreInstance
 
 admin.site.register(Chore)
+admin.site.register(ChoreAssignment)
 admin.site.register(ChoreInstance)
