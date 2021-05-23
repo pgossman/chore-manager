a71f48d450221fc2eddbccea917c0f287c682429
patmn <patmn@umich.edu>
2021-05-23 14:36:23 -0400

    turned off strict property intialization to eliminate need to initialize every variable. added Person.ts interface, changed db.json to people db instead of Lutherites to make more generalizable to other co-ops

---
+++ frontend/src/app/Person.ts
@@ -0,0 +1,9 @@
+export interface Person {
+  name: string;
+  chore1?: string;
+  chore2?: string;
+  chore3?: string;
+  chore4?: string;
+  chore5?: string;
+  chore6?: string;
+}
--- frontend/src/app/components/task-item/task-item.component.ts
+++ frontend/src/app/components/task-item/task-item.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Person } from 'src/app/Person';
 
 @Component({
   selector: 'app-task-item',
   templateUrl: './task-item.component.html',
-  styleUrls: ['./task-item.component.css']
+  styleUrls: ['./task-item.component.css'],
 })
 export class TaskItemComponent implements OnInit {
+  @Input() task: Person;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
